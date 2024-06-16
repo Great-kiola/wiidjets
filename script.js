e8058b0846edb296b6481eed43375f5601f31b6b
Ola <Hollabodey.kilaso@gmail.com>
2024-06-17 00:53:12 +0100

    Update weather API call in script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // Connect open weather API
- 
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    // Process the data here
+  })
+  .catch((error) => {
+    // Handle any errors here
+  });
