cfdf1e000d1881351b89d3b4a3c7c89bc38a45a4
Ola <hollabodey.kilaso@gmail.com>
2024-06-30 12:09:02 +0100

    Update script.js to log API response data

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Connect open weather API
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY"
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a461925d79fc440221121b90fd18ec1c"
 )
   .then((response) => response.json())
   .then((data) => {
     // Process the data here
+
+    console.log(data);
   })
   .catch((error) => {
     // Handle any errors here
