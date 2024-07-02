358601642825d2b1a559b5929869649c0baab366
Ola <hollabodey.kilaso@gmail.com>
2024-07-02 22:30:36 +0100

    feat: Add Crypto Card to index.html

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
+// All selector and pointers
+
 const cityTemp = document.querySelector("#temperature");
 const cityName = document.querySelector("#cityName");
 const humidity = document.querySelector(".humidity");
 
 // Connect open weather API
+
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a461925d79fc440221121b90fd18ec1c"
 )
-  .then((response) => response.json())
-  .then((data) => {
-    // Process the data here
+.then((response) => response.json())
+.then((data) => {  
+  
 
-    console.log(data);
+  console.log(data);
 
-    cityTemp.innerHTML = data.main.temp;
-    humidity.innerHTML = data.main.humidity;
-    cityName.innerHTML = data.name;
-  })
-  .catch((error) => {
-    // Handle any errors here
+  cityTemp.innerHTML = data.main.temp;
+  humidity.innerHTML = data.main.humidity;
+  cityName.innerHTML = data.name;
+})
+.catch((error) => {
     console.log("Not Sucessfull");
-  });
-
-
-  // Another unnecessary comment
-  // Another one
+});
   
 
