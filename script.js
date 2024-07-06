0b6bb10eba0338dbed89074361c1d2924ef0d7c5
Ola <hollabodey.kilaso@gmail.com>
2024-07-06 21:03:50 +0100

    refactor: Update script.js to display formatted date and improve code formatting

--- script.js
+++ script.js
@@ -4,25 +4,24 @@ const cityTemp = document.querySelector("#temperature");
 const cityName = document.querySelector("#cityName");
 const humidity = document.querySelector(".humidity");
 const normalDate = document.querySelector("#normalDate");
+const userAge = document.querySelector(".userAge");
 
 // Connect open weather API
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a461925d79fc440221121b90fd18ec1c"
 )
-.then((response) => response.json())
-.then((data) => {  
-  console.log(data);
-  cityTemp.innerHTML = data.main.temp;
-  humidity.innerHTML = data.main.humidity;
-  cityName.innerHTML = data.name;
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    cityTemp.innerHTML = data.main.temp;
+    humidity.innerHTML = data.main.humidity;
+    cityName.innerHTML = data.name;
+  })
+  .catch((error) => {
+    console.log("Error during fetch " + error.message);
+  });
 
-})
-.catch((error) => {
-  console.log("Error during fetch " + error.message);
-
-});
-  
-// Date 
+// Date
 const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth();
@@ -31,30 +30,21 @@ const dayofweek = date.getDay();
 
 normalDate.innerText = date.toDateString();
 
-console.log(date.toLocaleString('default' , {
-  month: 'long',
-  weekday: 'short'
-}));
-
+console.log(
+  date.toLocaleString("default", {
+    month: "long",
+    weekday: "short",
+  })
+);
 
 // Date of birth calculation
 console.log("The year is " + date.getFullYear());
+let userYear = prompt("What year were you born");
 
-
-
-// 1 
-const date3 = new Date();
-
-// 2
-const byMonth = date3.getMonth();f
-
-// 3
-const hour = date3.getHours();
-const min = date3.getMinutes();
-console.log(hour + ":" + min);
-
-// 4
-console.log(date3.toLocaleString("default" , {
-  weekday: "long"
-}));
-
+if (isNaN(userYear)) {
+  alert("Please enter a number")
+} else {
+  let userBirth = date.getFullYear() - userYear;
+  userAge.innerText = userBirth;
+  console.log(`You are ${userBirth} years old`);
+}
