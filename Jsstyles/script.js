692dc9263c902c7d1fae12cc4cbce863b4a42cf2
Ola <hollabodey.kilaso@gmail.com>
2024-11-22 15:58:30 +0000

    Refactor: Comment out weather API fetch and date calculations in script.js for future reference

--- Jsstyles/script.js
+++ Jsstyles/script.js
@@ -7,28 +7,28 @@ const normalDate = document.querySelector("#currDate");
 const userAge = document.querySelector(".userAge");
 
 // Connect open weather API
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a461925d79fc440221121b90fd18ec1c"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    // console.log(data);
-    cityTemp.innerHTML = data.main.temp;
-    humidity.innerHTML = data.main.humidity;
-    cityName.innerHTML = data.name;
-  })
-  .catch((error) => {
-    console.log("Error during fetch " + error.message);
-  });
+// fetch(
+//   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a461925d79fc440221121b90fd18ec1c"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // console.log(data);
+//     cityTemp.innerHTML = data.main.temp;
+//     humidity.innerHTML = data.main.humidity;
+//     cityName.innerHTML = data.name;
+//   })
+//   .catch((error) => {
+//     console.log("Error during fetch " + error.message);
+//   });
 
-// Date
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDate();
-const dayofweek = date.getDay();
+// // Date
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const dayofweek = date.getDay();
 
-normalDate.innerText = date.toDateString();
+// normalDate.innerText = date.toDateString();
 
 
 // Date of birth calculation
