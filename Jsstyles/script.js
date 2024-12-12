532a81981eff44baa895bcc9bf517730a2020e11
Ola <hollabodey.kilaso@gmail.com>
2024-12-12 02:14:23 +0000

    Add .env file for API key and update script.js for crypto price fetching

--- Jsstyles/script.js
+++ Jsstyles/script.js
@@ -1,24 +1,26 @@
-// All selector and pointers
+// require('dotenv').config();
 
+// All selector and pointers
 const cityTemp = document.querySelector("#temperature");
 const cityName = document.querySelector("#cityName");
 const humidity = document.querySelector(".humidity");
 const normalDate = document.querySelector("#currDate");
 const userAge = document.querySelector(".userAge").innerHTML;
+const btcPrice = document.querySelector(".btc-price");
 
 // Connect open weather API
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a461925d79fc440221121b90fd18ec1c"
 )
-.then((response) => response.json())
-.then((data) => {
-  cityTemp.innerHTML = data.main.temp;
-  humidity.innerHTML = data.main.humidity;
-  cityName.innerHTML = data.name;
-})
-.catch((error) => {
+  .then((response) => response.json())
+  .then((data) => {
+    cityTemp.innerHTML = data.main.temp;
+    humidity.innerHTML = data.main.humidity;
+    cityName.innerHTML = data.name;
+  })
+  .catch((error) => {
     console.log("Error during fetch " + error.message);
-});
+  });
 
 // Date
 const date = new Date();
@@ -27,11 +29,32 @@ const month = date.getMonth();
 const day = date.getDate();
 const dayofweek = date.getDay();
 
-function truncate(x, num){
-  if(x.length <= num){
-    return x;
-  } else{
-    return x.slice(0, num) + "...";
-  }
-}
+// Used to get the price of the crypto
+// fetch('https://rest.coinapi.io/v1/exchangerate/BTC/USD', {
+//   headers: {
+//       "X-CoinAPI-Key": "236eeb5c-1fbd-49c5-a9c6-4682ca522282" // Replace with your API key
+//       }
+//   })
+//   .then(response => response.json())
+//   .then((data) => {
+//     let price = data.rate
+//     let convertedPrice = price.toFixed(2)
+//     btcPrice.innerHTML = convertedPrice
+
+//   })
+//   .catch(error => console.error('Error:', error));
+
+
+// Used for the details of the crypto
+fetch("https://api.coinpaprika.com/v1/coins")
+  .then((res) => res.json())
+  .then((data) => {
+    for (let i = 0; i < 10; i++) {
+      console.log(data[i].name);
+    }
+  })
+  .catch((eRR) => {
+    console.log(eRR);
+  });
+
 
--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Extra features
 -- A relaxing box image.png users can click on that generates a random color like a light switch
 
 -- Add interval to make time count.
+
+
+
+
